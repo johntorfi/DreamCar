06bf82117af85cd70b45bcf9a3395388de7dc05f
ned mah <neda_mahavi@yahoo.com>
2019-10-19 11:30:48 +0100

    Add project files.

---
+++ DreamCar/Controllers/CarsController.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DreamCar.Models;
+
+namespace DreamCar.Controllers
+{
+    public class CarsController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        // GET: Cars
+        // public ActionResult Index()
+        //{
+        //  return View(db.Cars.ToList());
+        //}
+        public ActionResult Index(string Make, string searchString, string Model, string isChecked)
+        {
+            var MakLst = new List<string>();
+
+            var MakQry = from d in db.Cars
+                         orderby d.Make
+                         select d.Make;
+
+            MakLst.AddRange(MakQry.Distinct());
+            ViewBag.Make = new SelectList(MakLst);
+
+            var cars = from m in db.Cars
+                       select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                cars = cars.Where(s => s.Model.Contains(searchString));
+            }
+
+            if (!string.IsNullOrEmpty(Make))
+            {
+                cars = cars.Where(x => x.Make == Make);
+            }
+
+            if (isChecked == "on")
+
+            {
+                cars = from q in cars
+                       orderby q.Model
+                       select q;
+
+            }
+
+
+            return View(cars);
+
+        }
+
+
+
+
+
+
+
+
+        // GET: Cars/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // GET: Cars/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cars/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Make,Model,Engine,Specification,Price,ImageUrl")] Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cars.Add(car);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(car);
+        }
+
+        // GET: Cars/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // POST: Cars/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Make,Model,Engine,Specification,Price,ImageUrl")] Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(car).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(car);
+        }
+
+        // GET: Cars/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Car car = db.Cars.Find(id);
+            if (car == null)
+            {
+                return HttpNotFound();
+            }
+            return View(car);
+        }
+
+        // POST: Cars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Car car = db.Cars.Find(id);
+            db.Cars.Remove(car);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+
